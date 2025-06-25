3da29f0e7423b7c46dc346e3998890ac7807b2a5
张皓晟 <2067144018@qq.com>
2025-06-26 00:08:01 +0800

    1

--- stubgen/config.py
+++
@@ -1,7 +0,0 @@
-EXTENSION_API_PATH = 'godot-cpp/gdextension/extension_api.json' # Relative to the project root directory
-
-
-
-STUB_TYPINGS_PATH = "typings/godot/typings.pyi" # Relative to the project root directory
-EXPOSED_STUB_PATH = 'typings/godot/__init__.pyi' # Relative to the project root directory
-STUB_ENUM_PATH = 'typings/godot/enums.pyi' # Relative to the project root directory
