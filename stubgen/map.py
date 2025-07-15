fac8fcf5890134085c08eda8117ecab4810eb538
张皓晟 <2067144018@qq.com>
2025-07-16 00:21:55 +0800

    revert .

--- stubgen/map.py
+++ stubgen/map.py
@@ -21,27 +21,27 @@ class MapResult:
     c_writer: Writer
 
 
-def fill_converters(gdt_all_in_one: GodotInOne):  
-    '''
+def fill_converters(gdt_all_in_one: GodotInOne):
+    """
     使用gdt_all_in_one的信息初始化converters模块下的全局变量
-    '''
+    """
     converters.BUILTIN_TYPES.update(
         set(cls.name for cls in gdt_all_in_one.builtin_classes)
     )
-    
+
     converters.NATIVE_TYPES.update(set(cls.name for cls in gdt_all_in_one.classes))
-    
+
     for cls_data in gdt_all_in_one.classes + gdt_all_in_one.builtin_classes:
         if not cls_data.enums:
             continue
         cls_name = cls_data.name
-        enum_cls_name = f"{cls_name}StaticExtension"
-    
+        enum_cls_name = f"{cls_name}Enum"
+
         for enum_data in cls_data.enums:
             for v in enum_data.values or []:
-                # HTTPRequest, HTTPRequest.Result, HTTPRequestStaticExtension, Result, RESULT_SUCCESS, 0
-                # HTTPRequest, HTTPRequest.Result, HTTPRequestStaticExtension, Result, RESULT_CHUNKED_BODY_SIZE_MISMATCH, 1
-                # HTTPRequest, HTTPRequest.Result, HTTPRequestStaticExtension, Result, ..., ...
+                # HTTPRequest, HTTPRequest.Result, HTTPRequestEnum, Result, RESULT_SUCCESS, 0
+                # HTTPRequest, HTTPRequest.Result, HTTPRequestEnum, Result, RESULT_CHUNKED_BODY_SIZE_MISMATCH, 1
+                # HTTPRequest, HTTPRequest.Result, HTTPRequestEnum, Result, ..., ...
                 converters.append_records(
                     converters.CLASS_ENUM_DATA,
                     {
@@ -53,7 +53,7 @@ def fill_converters(gdt_all_in_one: GodotInOne):
                         "constant_value": v.value,
                     },
                 )
-    
+
     for enum in gdt_all_in_one.global_enums:
         if "." in enum.name:
             cls_name, enum_name = enum.name.split(".")
@@ -70,7 +70,7 @@ def fill_converters(gdt_all_in_one: GodotInOne):
                         "constant_value": v.value,
                     },
                 )
-    
+
         else:
             for v in enum.values:
                 # MethodFlags, MethodFlags, METHOD_FLAG_NORMAL, 1
@@ -85,7 +85,7 @@ def fill_converters(gdt_all_in_one: GodotInOne):
                         "constant_value": v.value,
                     },
                 )
-    
+
     for builtin_class in gdt_all_in_one.builtin_classes:
         cls_name = converters.convert_class_name(builtin_class.name)
         if builtin_class.operators:
@@ -101,12 +101,11 @@ def fill_converters(gdt_all_in_one: GodotInOne):
                             "op_name": op_name,  # 转换后的运算符名称
                         },
                     )
-    
-    
 
-def gen_c_writer(gdt_all_in_one:GodotInOne, c_writer: Writer) -> Writer:
+
+def gen_c_writer(gdt_all_in_one: GodotInOne, c_writer: Writer) -> Writer:
     c_writer.write(
-"""\
+        """\
 #include "Bindings.hpp"
 """
     )
@@ -133,22 +132,25 @@ def gen_c_writer(gdt_all_in_one:GodotInOne, c_writer: Writer) -> Writer:
         )
 
     for clazz in gdt_all_in_one.builtin_classes + gdt_all_in_one.classes:
-        variant_type = converters.CLASS_TO_VARIANT_TYPE.get(clazz.name, 'Variant::OBJECT')
+        variant_type = converters.CLASS_TO_VARIANT_TYPE.get(
+            clazz.name, "Variant::OBJECT"
+        )
         c_writer.write(f'register_GDNativeClass({variant_type}, "{clazz.name}");')
     for enum in gdt_all_in_one.global_enums:
         for v in enum.values:
             c_writer.write(f'register_GlobalConstant("{v.name}", {v.value});')
-    
+
     c_writer.dedent()
     c_writer.write("}")
     c_writer.write("")
     c_writer.write("} // namespace pkpy")
-    
+
     return c_writer
+
 
 def gen_typings_pyi_writer(gdt_all_in_one: GodotInOne, pyi_writer: Writer) -> Writer:
     pyi_writer.write(
-"""\
+        """\
 import typing
 from typing import overload
 from .enums import *
@@ -162,20 +164,20 @@ def default(gdt_expr: str) -> typing.Any: ...
 """
     )
     class_writer: Writer = pyi_writer
-    
+
     # -----手动定义Variant
     class_writer.write("class Variant:")
     class_writer.indent()
     class_writer.write("...")
     class_writer.dedent()
     class_writer.write("")
-    
+
     for clazz in gdt_all_in_one.builtin_classes + gdt_all_in_one.classes:
-    
+
         is_empty_class: bool = True
         # ------class xxx(xxx):
         class_name: str = converters.convert_class_name(clazz.name)
-    
+
         if isinstance(clazz, BuiltinClass):
             class_inherits = "Variant"
         else:
@@ -184,52 +186,52 @@ def default(gdt_expr: str) -> typing.Any: ...
                 if clazz.inherits
                 else None
             )
-    
+
         class_writer.writefmt(
             "class {0}{1}:", class_name, f"({class_inherits})" if class_inherits else ""
         )
-    
+
         class_writer.indent()
-    
+
         # ------Member variables
         if isinstance(clazz, BuiltinClass):
             member_vars = clazz.members
         else:
             member_vars = clazz.properties
-    
+
         if member_vars:
             for member_data in member_vars:
-    
+
                 member_name: str = converters.convert_keyword_name(member_data.name)
                 member_type: str = converters.convert_type_name(member_data.type)
-    
+
                 class_writer.writefmt("{0}: {1}", member_name, member_type)
-    
+
             class_writer.write("")
             is_empty_class = False
-    
+
         # ------Signals
         if isinstance(clazz, BuiltinClass):
             signals = None
         else:
             signals = clazz.signals
-    
+
             if signals:
-    
+
                 for signal in signals:
                     signal_name = converters.convert_keyword_name(signal.name)
                     signal_args = signal.arguments or []
-    
+
                     arg_comment_expr_list = []
                     arg_expr_list = []
                     for signal_arg in signal_args:
-    
+
                         arg_name = converters.convert_keyword_name(signal_arg.name)
                         arg_type = converters.convert_type_name(signal_arg.type)
-    
+
                         arg_comment_expr_list.append(arg_name + ":" + arg_type)
                         arg_expr_list.append(arg_type)
-    
+
                     class_writer.writefmt(
                         "{0}: Signal[typing.Callable[[{1}], None]]  # {2}",
                         signal.name,
@@ -240,46 +242,48 @@ def default(gdt_expr: str) -> typing.Any: ...
                             else "no arguments"
                         ),
                     )
-    
+
                 class_writer.write("")
                 is_empty_class = False
-                
+
         # ------Class variables
         if isinstance(clazz, BuiltinClass):
             class_consts = clazz.constants
         else:
             class_consts = clazz.constants
-    
+
         if class_consts:
             for const in class_consts:
                 const_name = converters.convert_keyword_name(const.name)
                 value = str(const.value)
                 class_writer.write(f"{const_name} = default({value!r})")  # TODO
-    
+
             class_writer.write("")
             is_empty_class = False
-    
+
         # ------Operators
         if isinstance(clazz, BuiltinClass):
             operators = clazz.operators
         else:
             operators = None
-    
+
         if operators:
             for operator in operators:
                 orign_operator_name = operator.name
-    
+
                 if converters.is_supported_operator(orign_operator_name):
                     operator_name = converters.convert_operator_to_method_name(
                         orign_operator_name
                     )
                     return_type = operator.return_type
                     right_type = operator.right_type
-                    right_type_is_alias = right_type in list(converters.ALIAS_CLASS_DATA.loc[:, "cls_name"])
-    
+                    right_type_is_alias = right_type in list(
+                        converters.ALIAS_CLASS_DATA.loc[:, "cls_name"]
+                    )
+
                     if converters.is_overload_operator(class_name, operator_name):
                         class_writer.write("@overload")
-    
+
                     if right_type:
                         alias_module_path = "alias"
                         if right_type_is_alias:
@@ -294,10 +298,10 @@ def default(gdt_expr: str) -> typing.Any: ...
                         class_writer.write(
                             f"def {operator_name}(self) -> {return_type}: ..."
                         )
-                        
+
             class_writer.write("")
             is_empty_class = False
-    
+
         # ------Class methods
         if clazz.methods:
             for method in clazz.methods:
@@ -311,38 +315,40 @@ def default(gdt_expr: str) -> typing.Any: ...
                         if method.return_type
                         else "None"
                     )
-    
+
                 # ------Arguments
                 arg_expr = []
                 if method.is_static:
                     class_writer.write("@staticmethod")
                 else:
                     arg_expr.append("self")
-    
+
                 for arg in method.arguments or []:
-    
+
                     arg_name = converters.convert_keyword_name(arg.name)
                     arg_type = converters.convert_type_name(arg.type)
-                    arg_type_is_alias = arg_type in list(converters.ALIAS_CLASS_DATA.loc[:, "cls_name"])
-                    
+                    arg_type_is_alias = arg_type in list(
+                        converters.ALIAS_CLASS_DATA.loc[:, "cls_name"]
+                    )
+
                     if arg.default_value:
                         arg_default_value = f"default({arg.default_value!r})"
                     else:
                         arg_default_value = None
-    
+
                     if arg_type_is_alias:
                         alias_module_path = "alias"
                         expr = f"{arg_name}: {alias_module_path}.{arg_type}"
                     else:
                         expr = f"{arg_name}: {arg_type}"
-                        
+
                     if arg.default_value is not None:
                         expr += f" = {arg_default_value}"
                     arg_expr.append(expr)
-    
+
                 if method.is_vararg:
                     arg_expr.append(f"*args")
-    
+
                 # ------Method
                 method_name = converters.convert_keyword_name(method.name)
                 class_writer.writefmt(
@@ -351,54 +357,62 @@ def default(gdt_expr: str) -> typing.Any: ...
                     ", ".join(arg_expr),
                     ret_t,
                 )
-    
+
             class_writer.write("")
             is_empty_class = False
-    
+
         if is_empty_class:
             class_writer.write("...")
         class_writer.dedent()
         class_writer.write("")
-    
-    
+
     return pyi_writer
 
+
 def gen_alias_pyi_writer(gdt_all_in_one: GodotInOne, pyi_writer: Writer) -> Writer:
-    
+
     modules = map(str, set(converters.ALIAS_CLASS_DATA.loc[:, "module_abs_path"]))
-    
+
     pyi_writer.write(
-f'''\
+        f"""\
 import {', '.join(modules)}
 from . import classes
 
 
-'''
+"""
     )
-    
+
     # 只有builtin_classes才有替用的类
     for clazz in gdt_all_in_one.builtin_classes:
         cls_name = converters.convert_class_name(clazz.name)
-        found_records = converters.find_records(converters.ALIAS_CLASS_DATA, {"cls_name":cls_name})
+        found_records = converters.find_records(
+            converters.ALIAS_CLASS_DATA, {"cls_name": cls_name}
+        )
         if len(found_records) > 0:
-            
-            alternative_cls_with_module_exprs: list[str] = []  # ["vmath.vec2", "vmath.vec3", ...]
+
+            alternative_cls_with_module_exprs: list[str] = (
+                []
+            )  # ["vmath.vec2", "vmath.vec3", ...]
             for _, record in found_records.iterrows():
-                alternative_cls_with_module_exprs.append(record.loc['module_abs_path'] + "." + record.loc['alternative_cls_name'])
-                
-            pyi_writer.writefmt("{0} = classes.{1} | {2}",
+                alternative_cls_with_module_exprs.append(
+                    record.loc["module_abs_path"]
+                    + "."
+                    + record.loc["alternative_cls_name"]
+                )
+
+            pyi_writer.writefmt(
+                "{0} = classes.{1} | {2}",
                 cls_name,
                 cls_name,
-                " | ".join(alternative_cls_with_module_exprs)
+                " | ".join(alternative_cls_with_module_exprs),
             )
-        
-    
+
     return pyi_writer
 
 
 def gen_init_pyi_writer(gdt_all_in_one: GodotInOne, pyi_writer: Writer) -> Writer:
     pyi_writer.write(
-"""\
+        """\
 from . import classes
 from . import enums
 
@@ -416,85 +430,87 @@ class GDBuiltinClass[T: classes.Variant]: ...
 
 """
     )
-    
+
     init_writer = pyi_writer
     for clazz in gdt_all_in_one.builtin_classes + gdt_all_in_one.classes:
         cls_name = converters.convert_class_name(clazz.name)
-        cls_type_name = converters.convert_class_name(clazz.name).split('[')[0]  # 忽略模板参数
-        records = converters.find_records(converters.CLASS_ENUM_DATA, {'cls_name': cls_name})
+        cls_type_name = converters.convert_class_name(clazz.name).split("[")[
+            0
+        ]  # 忽略模板参数
+        records = converters.find_records(
+            converters.CLASS_ENUM_DATA, {"cls_name": cls_name}
+        )
         cls_enum = None
         if len(records) > 0:
-            cls_enum = records.iloc[0].loc['cls_enum_name']
-        
+            cls_enum = records.iloc[0].loc["cls_enum_name"]
+
         if isinstance(clazz, BuiltinClass):
-            inherit_1 = f'GDBuiltinClass[classes.{cls_type_name}]'
+            inherit_1 = f"GDBuiltinClass[classes.{cls_type_name}]"
         else:
-            inherit_1 = f'GDNativeClass[classes.{cls_type_name}]'
-        
+            inherit_1 = f"GDNativeClass[classes.{cls_type_name}]"
+
         if cls_enum:
-            inherit_2 = f'enums.{cls_enum}'
+            inherit_2 = f"enums.{cls_enum}"
         else:
             inherit_2 = None
-        
+
         if inherit_2:
-            init_writer.write(f'class {cls_name}({inherit_1}, {inherit_2}): ...')
+            init_writer.write(f"class {cls_name}({inherit_1}, {inherit_2}): ...")
         else:
-            init_writer.write(f'class {cls_name}({inherit_1}): ...')
-    
-    
+            init_writer.write(f"class {cls_name}({inherit_1}): ...")
+
     return pyi_writer
 
 
 def gen_enums_pyi_writer(gdt_all_in_one: GodotInOne, pyi_writer: Writer) -> Writer:
     pyi_writer.write(
-"""\
+        """\
 from typing import Literal
 """
     )
     # --- global enum ---
     enum.gen_global_enums(pyi_writer, gdt_all_in_one.global_enums)
-    
+
     # --- classes enum ---
     enums_w = pyi_writer
-    
+
     for cls_data in gdt_all_in_one.classes + gdt_all_in_one.builtin_classes:
         if not cls_data.enums:
             continue
         cls_name = cls_data.name
-        enum_cls_name = f"{cls_name}StaticExtension"
+        enum_cls_name = f"{cls_name}Enum"
         enums_w.write(f"class {enum_cls_name}:")
         enums_w.indent()
-    
+
         for enum_data in cls_data.enums:
-    
-            if isinstance(enum_data, ClassesStaticExtension):
+
+            if isinstance(enum_data, ClassesEnum):
                 enum.gen_class_enum(enums_w, enum_data)
             else:
                 enum.gen_builtin_class_enum(enums_w, enum_data)
-    
+
         enums_w.dedent()
         enums_w.write("\n")
-    
+
     return pyi_writer
 
 
-
 def map_gdt_to_py(gdt_all_in_one: GodotInOne) -> MapResult:
-    
+
     map_result = MapResult(
-        typings_pyi= Writer(),
-        init_pyi= Writer(),
-        alias_pyi= Writer(),
-        enums_pyi= Writer(),
-        c_writer= Writer(), 
+        typings_pyi=Writer(),
+        init_pyi=Writer(),
+        alias_pyi=Writer(),
+        enums_pyi=Writer(),
+        c_writer=Writer(),
     )
 
     gen_c_writer(gdt_all_in_one, map_result.c_writer)  # 暂不依赖converters
-    
+
     fill_converters(gdt_all_in_one)
     gen_alias_pyi_writer(gdt_all_in_one, map_result.alias_pyi)
     gen_enums_pyi_writer(gdt_all_in_one, map_result.enums_pyi)
     gen_init_pyi_writer(gdt_all_in_one, map_result.init_pyi)
     gen_typings_pyi_writer(gdt_all_in_one, map_result.typings_pyi)
-    
+
     return map_result
