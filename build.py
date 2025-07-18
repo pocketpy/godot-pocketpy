cc91199bc001a90b8cce74a533c146f925d0d260
blueloveTH <blueloveTH@foxmail.com>
2025-07-18 13:54:23 +0800

    backup

--- build.py
+++ build.py
@@ -1,15 +1,30 @@
 import os
-import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--config', type=str, default='Debug')
+parser.add_argument('--platform', type=str, default='win32')
 
 def run(cmd: str):
     print(cmd)
     code = os.system(cmd)
     assert code == 0
 
-if len(sys.argv) > 1:
-    config = sys.argv[1]
-else:
-    config = 'Debug'
+args = parser.parse_args()
+config: str = args.config
+platform: str = args.platform
 
-run(f"cmake -B build -DCMAKE_BUILD_TYPE={config}")
+extra_flags = []
+
+if platform == 'android':
+    extra_flags.append('-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_HOME/build/cmake/android.toolchain.cmake')
+    extra_flags.append('-DANDROID_PLATFORM=android-22')
+    extra_flags.append('-DANDROID_ABI=arm64-v8a')
+elif platform == 'ios':
+    extra_flags.append('-DCMAKE_TOOLCHAIN_FILE=pocketpy/3rd/ios.toolchain.cmake')
+    extra_flags.append('-DDEPLOYMENT_TARGET=13.0')
+    extra_flags.append('-DPLATFORM=OS64')
+else:
+    run(f"cmake -B build -DCMAKE_BUILD_TYPE={config}")
+
 run(f"cmake --build build --config {config}")
