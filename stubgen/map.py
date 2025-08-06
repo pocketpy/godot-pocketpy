a3856c7cf7d90ecc757b405a739a80c33d1eefad
zhs628 <111758261+zhs628@users.noreply.github.com>
2025-08-06 16:11:27 +0800

    2025 08 06 maintenance (#14)
    * 将NodePath, StringName 加入到alias.py 例如alias.NodePath = NodePath | str
    
    * 通过多继承将类的常量/枚举桥接到类定义中(实现 class xxx(xxxx, xxxEnum))
    
    * 在pyrightconfig那里 ban 掉 reportOverlappingOverload (解决错误 “__mul__”的重载1与重载2重叠 并返回不兼容的类型 PylancereportOverlappingOverload)

--- stubgen/map.py
+++ stubgen/map.py
@@ -1,4 +1,6 @@
 from unittest import signals
+
+from pandas.core.frame import DataFrame
 from .schema_gdt import *
 
 from . import enum
@@ -198,24 +200,52 @@ def default(gdt_expr: str) -> typing.Any: ...
 
     for clazz in gdt_all_in_one.builtin_classes + gdt_all_in_one.classes:
         is_empty_class: bool = True
-        # ------class xxx(xxx):
+        # ------class xxx(xxx, xxxEnum):
         class_name: str = converters.convert_class_name(clazz.name)
 
         if class_name in converters.BLACKLIST_CLASS_NAMES:
             continue
-
+        
+        # 获取 super class
         if isinstance(clazz, BuiltinClass):
-            class_inherits = "Variant"
+            super_class_name = "Variant"
         else:
-            class_inherits = (
+            super_class_name = (
                 converters.convert_class_name(clazz.inherits)
                 if clazz.inherits
                 else None
             )
 
-        writer.writefmt(
-            "class {0}{1}:", class_name, f"({class_inherits})" if class_inherits else ""
-        )
+        # 获取 xxxEnum
+        class_enum_name = None
+        found_enum_records = converters.find_records(
+                    converters.CLASS_ENUM_DATA,
+                    {"cls_name": clazz.name},
+                )
+        if found_enum_records.shape[0] > 0:
+            class_enum_name = found_enum_records.iloc[0]["cls_enum_name"]
+        
+        # 组装 (xxx, xxxEnum)
+        inherts = []
+        if super_class_name is not None:
+            inherts.append(super_class_name)
+        if class_enum_name is not None:
+            inherts.append(class_enum_name)
+        
+        inhert_str = None
+        if inherts != []:
+            inhert_str = ", ".join(inherts)
+        
+        # 组装 class xxx(xxx, xxxEnum)
+        if inhert_str is not None:
+            writer.writefmt("class {0}{1}:", 
+                class_name, 
+                f"({inhert_str})"
+            )
+        else:
+            writer.writefmt("class {0}:", 
+                class_name
+            )
 
         writer.indent()
 
@@ -450,7 +480,8 @@ def default(gdt_expr: str) -> typing.Any: ...
 def gen_alias_pyi_writer(gdt_all_in_one: GodotInOne, pyi_writer: Writer) -> Writer:
 
     modules = map(str, set(converters.ALIAS_CLASS_DATA.loc[:, "module_abs_path"]))
-
+    modules = [module for module in modules if module != ""]
+    
     pyi_writer.write(
         f"""\
 import {', '.join(modules)}
@@ -472,11 +503,18 @@ from . import classes
                 []
             )  # ["vmath.vec2", "vmath.vec3", ...]
             for _, record in found_records.iterrows():
-                alternative_cls_with_module_exprs.append(
-                    record.loc["module_abs_path"]
-                    + "."
-                    + record.loc["alternative_cls_name"]
-                )
+                module = record.loc["module_abs_path"] if record.loc["module_abs_path"] != "" else None
+                
+                if module is None:
+                    alternative_cls_with_module_exprs.append(
+                        record.loc["alternative_cls_name"]
+                    )
+                else:
+                    alternative_cls_with_module_exprs.append(
+                        record.loc["module_abs_path"]
+                        + "."
+                        + record.loc["alternative_cls_name"]
+                    )
 
             pyi_writer.writefmt(
                 "{0} = classes.{1} | {2}",
